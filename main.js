1a1d302ab46efb84651d971e9042d2d8b31a2e93
jaydeepnaiupforce <jaydeep.nai@upforce.tech>
2024-06-11 15:25:20 +0530

    final commits on thinkpad

--- main.js
+++ main.js
@@ -1,47 +1,32 @@
-const { app, ipcMain } = require("electron");
-const WindowManager = require("./tray/Helper");
-
-win = new WindowManager();
-
-app.on("ready",win.createUI.bind(win))
-
-
-ipcMain.on("create-tray",()=>{
-  win.createTray(win)
-})
-
-ipcMain.on("remove-tray",()=>{
-  win.tray.destroy()
-})
-
-const getTrayCoordinates = ()=>{
-  const windowBounds = win.win.getBounds();
-  const trayBounds = win.tray.getBounds();
-
-  
-  let x = 0;
-  let y = 0;
-
-  x = Math.round(
-    trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
-  );
-  y = Math.round(trayBounds.y - windowBounds.height);
-
-  return {
-    x: x,
-    y: y,
-  };
-
-}
+const { app, ipcMain, BrowserWindow } = require("electron");
+const { dialog } = require('electron');
+const { AbortController } = require('abort-controller');
+const controller = new AbortController();
+const signal = controller.signal;
 
 
+var win ; 
 
-ipcMain.on('getTrayCoordinates', (event) => {
-  const trayBounds = win.tray.getBounds();
-  event.reply('trayCoordinates', trayBounds);
-});
+app.on("ready",()=>{
+  win = new BrowserWindow({
+    width: 250,
+    height: 435,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      plugins: true,
+    },
+  });
 
+  win.loadFile("./index.html");
 
-ipcMain.on("hideWindow",()=>{
-  win.win.hide()
+     dialog.showMessageBox({
+      type: 'info',
+      title: 'Information',
+      message: 'This is an informational message.',
+      buttons: ['Yes', 'No'],
+      defaultId:2,
+      signal: signal // Pass the signal to the message box
+    })
+    
 })
